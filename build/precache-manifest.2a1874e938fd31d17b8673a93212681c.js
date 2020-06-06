71bfc60d91c09b54288a7c1a0717c14eef476344
Brandon Mino <43628774+brandonmino@users.noreply.github.com>
2020-06-06 16:41:25 -0400

    add build

---
+++ build/precache-manifest.2a1874e938fd31d17b8673a93212681c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3650a58d897869c1884c300d9e7a78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6702ea5c17750135206d",
+    "url": "/static/css/main.bd2cdb9d.chunk.css"
+  },
+  {
+    "revision": "d7d677106eb01b7fa6fd",
+    "url": "/static/js/2.2489748e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2489748e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6702ea5c17750135206d",
+    "url": "/static/js/main.c57156d4.chunk.js"
+  },
+  {
+    "revision": "19254fdd2fb1863001ac",
+    "url": "/static/js/runtime-main.736320c0.js"
+  },
+  {
+    "revision": "d491653ec825055658ead27c6e71ee71",
+    "url": "/static/media/logo.d491653e.png"
+  },
+  {
+    "revision": "9d5303a7ed10e54d1f9216beb2d33b07",
+    "url": "/static/media/placeholder1.9d5303a7.jpg"
+  },
+  {
+    "revision": "9457ccf4b0ecb91304a6e3eca7acf42b",
+    "url": "/static/media/placeholder2.9457ccf4.jpg"
+  }
+]);
