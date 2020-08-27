014dbe33cca281590f75c6a3800411be2fe6e7cd
brandonmino <43628774+brandonmino@users.noreply.github.com>
2020-08-27 13:43:08 -0400

    Update components and git ignore to include vscode file

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function About() {
+export default function Calendar() {
     return (
         <div>
             <h1>Calendar</h1>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function About() {
+export default function Contact() {
     return (
         <div>
             <h1>Contact Us</h1>
