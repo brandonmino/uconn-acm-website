62e8f61964f2e0cc3b1313c3f45be2c52f00bd4f
brandonmino <brandon.mino@uconn.edu>
2020-09-07 17:46:25 -0400

    update logos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import mslogo from '../../assets/MSLearn.png';
+import rmlogo from '../../assets/ripplematchlogo.png';
 import './style.css';
 
 export default function Footer() {
@@ -8,8 +9,15 @@ export default function Footer() {
       <div className="footer-row">
         <div className="footer-column">
           <div className="logo-container">
-            <img src={logo} className="logo" alt="footer-logo"/>
-            <img src={mslogo} className="logo" alt="footer-logo"/>
+            <div className="image-container">
+              <img src={rmlogo} className="logo" alt="footer-rm-logo"/>
+            </div>
+            <div className="image-container">
+              <img src={logo} className="logo" color="transparent" alt="footer-logo"/>
+            </div>
+            <div className="image-container">
+              <img src={mslogo} className="logo" alt="footer-ms-logo"/>
+            </div>
           </div>
           <div className="resource-paragraph">
             <p>Association for Computing Machinery <br/>
