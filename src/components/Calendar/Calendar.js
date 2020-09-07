d97febb20d5765eea731a6215d40dcbe0da65da6
brandonmino <brandon.mino@uconn.edu>
2020-09-07 10:48:23 -0400

    update calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -12,74 +12,68 @@ export default function Calendar(props) {
                 <p className="header-text">All general body meetings are on Tuesdays on 7pm via Microsoft Teams. To join our Team, click {teamsLink}</p>
             </div>
             <div className="calendar-container">
-                <div className="item-container top-item-container">
-                    <p className="calendar-text-header">Week 1 / September 8: &nbsp;</p>
+                <div className="item-container">
+                    <p className="calendar-text-header">Week 1 / September 15: &nbsp;</p>
                     <p className="calendar-text-body">Introduction to ACM</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 2 / September 15: &nbsp;</p>
-                    <p className="calendar-text-body">Microsoft Learn Workshop</p>
-                </div>
-            </div>
-            <div className="calendar-container">
-                <div className="item-container">
-                    <p className="calendar-text-header">Week 2 / September 16: &nbsp;</p>
+                    <p className="calendar-text-header">Week 1 / September 16: &nbsp;</p>
                     <p className="calendar-text-body">UPE & ACM Cigna Event</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 3 / September 22: &nbsp;</p>
+                    <p className="calendar-text-header">Week 2 / September 22: &nbsp;</p>
                     <p className="calendar-text-body">Fast Enterprise Info Session & Resume Critique</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 4 / September 29: &nbsp;</p>
+                    <p className="calendar-text-header">Week 3 / September 29: &nbsp;</p>
                     <p className="calendar-text-body">Cigna Big Data Workshop</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 5 / October 6: &nbsp;</p>
+                    <p className="calendar-text-header">Week 4 / October 6: &nbsp;</p>
                     <p className="calendar-text-body">Travelers Workshop</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 6 / October 13: &nbsp;</p>
-                    <p className="calendar-text-body">TBD</p>
+                    <p className="calendar-text-header">Week 5 / October 13: &nbsp;</p>
+                    <p className="calendar-text-body">Microsoft Learn Workshop</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 7 / October 20: &nbsp;</p>
+                    <p className="calendar-text-header">Week 6 / October 20: &nbsp;</p>
                     <p className="calendar-text-body">Brooksource Info Session and Interview Tips</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 8 / October 27: &nbsp;</p>
+                    <p className="calendar-text-header">Week 7 / October 27: &nbsp;</p>
                     <p className="calendar-text-body">TBD</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 9 / November 3: &nbsp;</p>
+                    <p className="calendar-text-header">Week 8 / November 3: &nbsp;</p>
                     <p className="calendar-text-body">TBD</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 10 / November 10: &nbsp;</p>
+                    <p className="calendar-text-header">Week 9 / November 10: &nbsp;</p>
                     <p className="calendar-text-body">TBD</p>
                 </div>
             </div>
             <div className="calendar-container">
                 <div className="item-container">
-                    <p className="calendar-text-header">Week 11 / November 17: &nbsp;</p>
+                    <p className="calendar-text-header">Week 10 / November 17: &nbsp;</p>
                     <p className="calendar-text-body">TBD</p>
                 </div>
             </div>
